714830054259095684621ec76705a6b8c795067e
siricher <71574242+siricher@users.noreply.github.com>
2020-12-22 10:36:44 -0600

    Facebook Login Added

--- app/src/main/java/com/siricherukuri/manavata/MainActivity.java
+++ app/src/main/java/com/siricherukuri/manavata/MainActivity.java
@@ -8,29 +8,15 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
-    private Button log_in_sign_up;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        log_in_sign_up = (Button) findViewById(R.id.log_in_sign_up);
-        log_in_sign_up.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openSignInActivity();
-            }
-        });
-
-    }
-    public void openSignInActivity(){
-        Intent intentSI = new Intent(this, SignInActivity.class);
-        startActivity(intentSI);
+        defineButton();
     }
 
-    public void defineButtons() {
+    public void defineButton() {
         findViewById(R.id.log_in_sign_up).setOnClickListener(buttonClickListener);
     }
 
@@ -39,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
         public void onClick(View view) {
             switch (view.getId()) {
                 case R.id.log_in_sign_up:
-                    startActivity(new Intent(MainActivity.this,SignInActivity.class));
+                    startActivity(new Intent(MainActivity.this, SignInActivity.class));
                     break;
             }
         }
--- app/src/main/java/com/siricherukuri/manavata/SignInActivity.java
+++ app/src/main/java/com/siricherukuri/manavata/SignInActivity.java
@@ -2,53 +2,156 @@ package com.siricherukuri.manavata;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.squareup.picasso.Picasso;
 
 public class SignInActivity extends MainActivity {
-
-    private Button cancelbutton;
+    private CallbackManager mCallBackManager;
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private TextView textViewUser;
+    private ImageView mLogo;
+    private LoginButton loginButton;
+    private AccessTokenTracker accessTokenTracker;
+    private static String TAG = "FacebookAuthentication";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
 
-        cancelbutton = (Button) findViewById(R.id.cancelbutton);
-        cancelbutton.setOnClickListener(new View.OnClickListener() {
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        FacebookSdk.sdkInitialize(getApplicationContext());
+
+        textViewUser = findViewById(R.id.log_in_name);
+        mLogo = findViewById(R.id.logo_picture);
+        loginButton = findViewById(R.id.login_button_facebook);
+        loginButton.setReadPermissions("email", "public_profile");
+        mCallBackManager = CallbackManager.Factory.create();
+        loginButton.registerCallback(mCallBackManager, new FacebookCallback<LoginResult>() {
             @Override
-            public void onClick(View view) {
-                openHomeScreen();
+            public void onSuccess(LoginResult loginResult) {
+                Log.d(TAG, "onSuccess" + loginResult);
+                handleFacebookToken(loginResult.getAccessToken());
+
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d(TAG, "onCancel");
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.d(TAG, "onError + error");
+
             }
         });
 
-        setContentView(R.layout.activity_sign_in);
-        defineButtons();
+
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    updateUI(user);
+
+                } else {
+                    updateUI(null);
+                }
+            }
+        };
+
+        accessTokenTracker = new AccessTokenTracker() {
+            @Override
+            protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
+                if (currentAccessToken == null) {
+                    mFirebaseAuth.signOut();
+                }
+            }
+        };
     }
 
-    public void defineButtons() {
-        findViewById(R.id.googleemailbutton).setOnClickListener(buttonClickListener);
-        findViewById(R.id.facebookbutton).setOnClickListener(buttonClickListener);
-    }
+        private void handleFacebookToken (AccessToken token){
+            Log.d(TAG, "handleFacebookToken" + token);
 
-    public void openHomeScreen(){
-        Intent intentHS = new Intent(this, HomeScreen.class);
-        startActivity(intentHS);
-    }
+            AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+            mFirebaseAuth.signInWithCredential(credential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        Log.d(TAG, "Sign in with credential: successful");
+                        FirebaseUser user = mFirebaseAuth.getCurrentUser();
+                        updateUI(user);
+                    } else {
+                        Log.d(TAG, "Sign in with credential: failure", task.getException());
+                        updateUI(null);
+                    }
+                }
+            });
 
-    private View.OnClickListener buttonClickListener = new View.OnClickListener() {
+
+        }
+
         @Override
-        public void onClick (View view) {
-            switch (view.getId()) {
-                case R.id.googleemailbutton:
-                    startActivity(new Intent(SignInActivity.this, GoogleEmailActivity.class));
-                    break;
-                case R.id.facebookbutton:
-                    startActivity(new Intent(SignInActivity.this, FacebookActivity.class));
-                    break;
+        protected void onActivityResult ( int requestCode, int resultCode, @Nullable Intent data){
+            mCallBackManager.onActivityResult(requestCode, resultCode, data);
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+
+        private void updateUI (FirebaseUser user){
+            if (user != null) {
+                textViewUser.setText(user.getDisplayName());
+                if (user.getPhotoUrl() != null) {
+                    String photoUrl = user.getPhotoUrl().toString();
+                    photoUrl = photoUrl + "?type=large";
+                    Picasso.get().load(photoUrl).into(mLogo);
+                } else {
+                    textViewUser.setText("");
+                    mLogo.setImageResource(R.drawable.facebooklogo);
+                }
             }
         }
-    };
 
-}
+        @Override
+        protected void onStart () {
+            super.onStart();
+            mFirebaseAuth.addAuthStateListener(authStateListener);
+        }
+
+        @Override
+        protected void onStop () {
+            super.onStop();
+            if (authStateListener != null) {
+                mFirebaseAuth.removeAuthStateListener(authStateListener);
+            }
+        }
+    }
+
 
