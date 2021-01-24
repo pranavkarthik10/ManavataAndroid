271bb27711fc838674a551dbc7fa7dd67b13139a
Neerajakshi Daggubati <neerajakshid@users.noreply.github.com>
2021-01-23 17:53:29 -0800

    Fixing issues with Facebook authentication when with google

--- app/src/main/java/com/siricherukuri/manavata/HomeScreen.java
+++ app/src/main/java/com/siricherukuri/manavata/HomeScreen.java
@@ -19,151 +19,239 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.material.navigation.NavigationView;
 
-public class HomeScreen extends MainActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class SignInActivity extends MainActivity {
+    private CallbackManager mCallBackManager;
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private TextView textViewUser;
+    private ImageView mLogo;
+    private LoginButton facebookLogin;
+    private AccessTokenTracker accessTokenTracker;
+    private static String TAG = "FacebookandGoogleAuthentication";
+    private Bundle savedInstanceState;
+    private FirebaseUser user;
 
-    private Button idLC;
-    private Button idAM;
-    private Button idY;
-    private Button idHC;
-    private Button idPF;
-    private Button nav_signout;
+    private SignInButton googleSignIn;
     private GoogleSignInClient mGoogleSignInClient;
+    private FirebaseAuth mAuth;
+    private Button btnSignOut;
+    private int RC_SIGN_IN = 1;
 
-    DrawerLayout drawerLayout;
-    NavigationView navigationView;
-    Toolbar toolbar;
+    private Button mbypass;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_screen);
+        setContentView(R.layout.activity_sign_in);
 
-        drawerLayout = findViewById(R.id.homescreendrawerlayout);
-        navigationView = findViewById(R.id.nav_view);
-        toolbar = findViewById(R.id.rohanstoolbar);
-
-
-        setSupportActionBar(toolbar);
-
-        navigationView.bringToFront();
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
-        drawerLayout.addDrawerListener(toggle);
-        toggle.syncState();
-
-        navigationView.setNavigationItemSelectedListener(this);
-
-        idLC = findViewById(R.id.idLC);
-        idLC.setOnClickListener(new View.OnClickListener() {
+        mbypass = findViewById(R.id.bypass);
+        mbypass.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                openLifeCoachButton();
+                Intent i = new Intent(SignInActivity.this, HomeScreen.class);
+                startActivity(i);
             }
         });
 
-        idPF = findViewById(R.id.idPF);
-        String name = getIntent().getStringExtra("userDisplayName");
-        idPF.setText("Welcome " + name);
+        facebookAuthSetup();
 
-        View headerView = navigationView.getHeaderView(0);
-        TextView displayName = headerView.findViewById(R.id.displayname);
-        displayName.setText(name);
 
-        idAM = findViewById(R.id.idAM);
-        idAM.setOnClickListener(new View.OnClickListener() {
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    updateUI(user);
+
+                } else {
+                    updateUI(null);
+                }
+            }
+        };
+
+        accessTokenTracker = new AccessTokenTracker() {
+            @Override
+            protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
+                if (currentAccessToken == null) {
+                    mFirebaseAuth.signOut();
+                }
+            }
+        };
+
+
+        googleSignIn = findViewById(R.id.login_button_google);
+        mAuth = FirebaseAuth.getInstance();
+        btnSignOut = findViewById(R.id.log_out_google);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(SignInActivity.this, gso);
+
+        googleSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                openAboutManavata();
+                signIn();
             }
         });
 
-        idY = findViewById(R.id.idY);
-        idY.setOnClickListener(new View.OnClickListener() {
+
+        btnSignOut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                openYoga();
+                mGoogleSignInClient.signOut();
+                Toast.makeText(SignInActivity.this, "You are Logged Out", Toast.LENGTH_SHORT).show();
             }
         });
 
-        idHC = findViewById(R.id.idHC);
-        idHC.setOnClickListener(new View.OnClickListener() {
+    }
+
+    private void facebookAuthSetup() {
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        FacebookSdk.sdkInitialize(getApplicationContext());
+
+        textViewUser = findViewById(R.id.log_in_name);
+        mLogo = findViewById(R.id.logo_picture);
+        facebookLogin = findViewById(R.id.login_button_facebook);
+        facebookLogin.setReadPermissions("email", "public_profile");
+        mCallBackManager = CallbackManager.Factory.create();
+        facebookLogin.registerCallback(mCallBackManager, new FacebookCallback<LoginResult>() {
             @Override
-            public void onClick(View view) {
-                openHealthyCooking();
+            public void onSuccess(LoginResult loginResult) {
+                Log.d(TAG, "onSuccess" + loginResult);
+                handleFacebookToken(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d(TAG, "onCancel");
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.d(TAG, "onError + error");
             }
         });
+    }
+
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
     }
 
     @Override
-    public void onBackPressed() {
-        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
-            drawerLayout.closeDrawer(GravityCompat.START);
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        } else {
+            mCallBackManager.onActivityResult(requestCode, resultCode, data);
+            super.onActivityResult(requestCode, resultCode, data);
         }
-        else {
-            super.onBackPressed();
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount acc = completedTask.getResult(ApiException.class);
+            Toast.makeText(SignInActivity.this, "Signed In Successfully", Toast.LENGTH_SHORT).show();
+            FirebaseGoogleAuth(acc.getIdToken());
+        } catch (ApiException e) {
+            Toast.makeText(SignInActivity.this, "Sign In Failed", Toast.LENGTH_SHORT).show();
+            FirebaseGoogleAuth(null);
         }
 
     }
 
-    public void openLifeCoachButton() {
-        Intent intentLC = new Intent(this, lifecoachbutton.class);
-        startActivity(intentLC);
+    public void openHomeScreen(String userDisplayName) {
+        Intent intentHS = new Intent(SignInActivity.this, HomeScreen.class);
+        intentHS.putExtra("userDisplayName", userDisplayName);
+        startActivity(intentHS);
     }
 
-    public void openAboutManavata() {
-        Intent intentAM = new Intent(this, AboutManavataActivity.class);
-        startActivity(intentAM);
+    private void FirebaseGoogleAuth(String token) {
+        AuthCredential authCredential = GoogleAuthProvider.getCredential(token, null);
+        mAuth.signInWithCredential(authCredential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(SignInActivity.this, "Successful", Toast.LENGTH_SHORT).show();
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    updateUI(user);
+
+                    openHomeScreen(user.getDisplayName());
+
+                } else {
+                    Toast.makeText(SignInActivity.this, "Failed", Toast.LENGTH_SHORT).show();
+                    updateUI(null);
+                }
+            }
+        });
     }
-    public void openYoga() {
-        Intent intentY = new Intent(this, YogaActivity.class);
-        startActivity(intentY);
+
+
+    private void handleFacebookToken(AccessToken token) {
+        Log.d(TAG, "handleFacebookToken" + token);
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        mFirebaseAuth.signInWithCredential(credential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Log.d(TAG, "Sign in with credential: successful");
+                    user = mFirebaseAuth.getCurrentUser();
+                    updateUI(user);
+                    openHomeScreen(user.getDisplayName());
+                } else {
+                    Log.d(TAG, "Sign in with credential: failure", task.getException());
+                    updategoogleUI(null);
+                }
+            }
+        });
     }
-    public void openHealthyCooking() {
-        Intent intentHC = new Intent(this, HealthyCookingActivity.class);
-        startActivity(intentHC);
+
+
+    private void updateUI(FirebaseUser user) {
+        if (user != null) {
+            textViewUser.setText(user.getDisplayName());
+            if (user.getPhotoUrl() != null) {
+                String photoUrl = user.getPhotoUrl().toString();
+                photoUrl = photoUrl + "?type=large";
+                Picasso.get().load(photoUrl).into(mLogo);
+            } else {
+                textViewUser.setText("");
+                mLogo.setImageResource(R.drawable.facebooklogo);
+            }
+        }
+    }
+
+    private void updategoogleUI(FirebaseUser fUser) {
+        btnSignOut.setVisibility(View.VISIBLE);
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(getApplicationContext());
+        if (account != null) {
+            String personName = account.getDisplayName();
+            Uri personPhoto = account.getPhotoUrl();
+
+            Toast.makeText(SignInActivity.this, personName + personPhoto, Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+    protected void onStart() {
+        super.onStart();
+        mFirebaseAuth.addAuthStateListener(authStateListener);
+    }
 
-        switch (menuItem.getItemId()){
-            case  R.id.nav_lifecoach:
-                Intent intentnLC = new Intent(HomeScreen.this,lifecoachbutton.class);
-                startActivity(intentnLC);
-                break;
-
-            case  R.id.nav_yoga:
-                Intent intentnY = new Intent(HomeScreen.this,YogaActivity.class);
-                startActivity(intentnY);
-                break;
-
-            case  R.id.nav_healthycooking:
-                Intent intentnHC = new Intent(HomeScreen.this,HealthyCookingActivity.class);
-                startActivity(intentnHC);
-                break;
-
-            case  R.id.nav_manavata:
-                Intent intentnAM = new Intent(HomeScreen.this,AboutManavataActivity.class);
-                startActivity(intentnAM);
-                break;
-                
-             case R.id.nav_donate:
-                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://manavata.org/donate-now/"));
-                startActivity(browserIntent);
-                break;
-
-                
-            case  R.id.nav_contact:
-                Intent intentnC = new Intent(HomeScreen.this,ContactUsActivity.class);
-                startActivity(intentnC);
-                break;
-
-            case R.id.nav_signout:
-                Intent intentnSO = new Intent(HomeScreen.this,MainActivity.class);
-                startActivity(intentnSO);
-                break;
-
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (authStateListener != null) {
+            mFirebaseAuth.removeAuthStateListener(authStateListener);
         }
-
-        return true;
     }
 }
