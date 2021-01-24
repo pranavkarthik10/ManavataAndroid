bacccea8357beffe2901aa5f1b6a1d106653a1a6
Neerajakshi Daggubati <neerajakshid@users.noreply.github.com>
2021-01-23 20:18:29 -0800

    Update SignInActivity.java

--- app/src/main/java/com/siricherukuri/manavata/SignInActivity.java
+++ app/src/main/java/com/siricherukuri/manavata/SignInActivity.java
@@ -11,7 +11,6 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
