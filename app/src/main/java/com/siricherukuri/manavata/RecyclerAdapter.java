d2cedc6db2a07fce395f7f166616fbd38d7477fc
Neerajakshi Daggubati <neerajakshid@users.noreply.github.com>
2021-01-29 17:50:16 -0800

    Update RecyclerAdapter.java

--- app/src/main/java/com/siricherukuri/manavata/RecyclerAdapter.java
+++ app/src/main/java/com/siricherukuri/manavata/RecyclerAdapter.java
@@ -15,6 +15,7 @@ import android.widget.TimePicker;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 
 public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHolder> {
@@ -25,15 +26,19 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHo
     int currentMinute;
     boolean isPM;
     Context mcontext;
+    String currentSchedule = "";
+    ArrayList<String> dailyScheduleList = new ArrayList<String>();
 
+    MySharedPreferences preferences;
     private static final String TAG = "RecyclerAdapter";
 
     private int [] alarmimagebuttons;
 
     public RecyclerAdapter (int[] alarmimagebuttons, Context context){
-
         this.alarmimagebuttons = alarmimagebuttons;
         this.mcontext = context;
+
+        preferences = new MySharedPreferences(context);
     }
 
     @NonNull
@@ -52,14 +57,18 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHo
     @Override
     public void onBindViewHolder(@NonNull final ViewHolder holder, int position)
     {
+        final int currentPosition = position;
+        dailyScheduleList.clear();
+        dailyScheduleList = preferences.getDailySchedule();
+        currentSchedule = dailyScheduleList.get(currentPosition);
+
+
         int image_id = alarmimagebuttons [position];
         holder.alarmholder.setImageResource(image_id);
 
-        calendar = Calendar.getInstance();
-        currentHour = calendar.get(Calendar.HOUR_OF_DAY);
-        currentMinute = calendar.get(Calendar.MINUTE);
-        isPM = (currentHour >= 12);
-
+        if(!currentSchedule.equals("")) {
+            holder.timeholder.setText(currentSchedule);
+        }
 
         holder.alarmholder.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -68,11 +77,14 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHo
                     @Override
                     public void onTimeSet(TimePicker timePicker, int hourOfday, int minutes) {
                         isPM = (hourOfday >= 12);
-                        holder.timeholder.setText(String.format("%02d:%02d %s", (hourOfday == 12 || hourOfday == 0) ? 12 : hourOfday % 12, minutes, isPM ? "PM" : "AM"));
+                        currentSchedule = String.format("%02d:%02d %s", (hourOfday == 12 || hourOfday == 0) ? 12 : hourOfday % 12, minutes, isPM ? "PM" : "AM");
+                        holder.timeholder.setText(currentSchedule);
+                        dailyScheduleList.set(currentPosition, currentSchedule);
+                        preferences.saveDailySchedule(dailyScheduleList);
 
                         Intent intent = new Intent(AlarmClock.ACTION_SET_ALARM);
-                        intent.putExtra(AlarmClock.EXTRA_HOUR, hourOfday);
-                        intent.putExtra(AlarmClock.EXTRA_MINUTES, minutes);
+                        intent.putExtra(AlarmClock.EXTRA_HOUR, currentHour);
+                        intent.putExtra(AlarmClock.EXTRA_MINUTES, currentMinute);
                         mcontext.startActivity(intent);
                     }
                 }, currentHour, currentMinute, false);
