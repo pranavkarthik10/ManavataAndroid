cd257e1ddce9f9fc56a6491709eb6829833c93d7
Neerajakshi Daggubati <neerajakshid@users.noreply.github.com>
2021-01-30 19:38:25 -0800

    Update WhatDoWeDoActivity.java

--- app/src/main/java/com/siricherukuri/manavata/WhatDoWeDoActivity.java
+++ app/src/main/java/com/siricherukuri/manavata/WhatDoWeDoActivity.java
@@ -3,7 +3,9 @@ package com.siricherukuri.manavata;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.View;
+import android.widget.TextView;
 
 
 public class WhatDoWeDoActivity extends MainActivity {
@@ -11,67 +13,63 @@ public class WhatDoWeDoActivity extends MainActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.what_do_we_do);
+
+        TextView samskaravidya = findViewById(R.id.samskaravidya);
+        samskaravidya.setText(Html.fromHtml("<u>" + samskaravidya.getText() + "</u>"));
+        samskaravidya.setOnClickListener(buttonClickListener);
+
+        TextView yogaandhealthcare = findViewById(R.id.yogaandhealthcare);
+        yogaandhealthcare.setText(Html.fromHtml("<u>" + yogaandhealthcare.getText() + "</u>"));
+        yogaandhealthcare.setOnClickListener(buttonClickListener);
+
+        TextView universityforhumanity = findViewById(R.id.universityforhumanity);
+        universityforhumanity.setText(Html.fromHtml("<u>" + universityforhumanity.getText() + "</u>"));
+        universityforhumanity.setOnClickListener(buttonClickListener);
+
+        TextView ashramandchildcare = findViewById(R.id.ashramandchildcare);
+        ashramandchildcare.setText(Html.fromHtml("<u>" + ashramandchildcare.getText() + "</u>"));
+        ashramandchildcare.setOnClickListener(buttonClickListener);
+
+        TextView helpneedy = findViewById(R.id.helpneedy);
+        helpneedy.setText(Html.fromHtml("<u>" + helpneedy.getText() + "</u>"));
+        helpneedy.setOnClickListener(buttonClickListener);
+
+        TextView climatechange = findViewById(R.id.climatechange);
+        climatechange.setText(Html.fromHtml("<u>" + climatechange.getText() + "</u>"));
+        climatechange.setOnClickListener(buttonClickListener);
     }
-    public void MyWeb(View view)
-    {
-        openUrl("https://manavata.org/education-project/");
-    }
-    public void openUrl (String url)
-    {
-        Uri uri=Uri.parse(url);
-        Intent launchWeb=new Intent(Intent. ACTION_VIEW, uri);
-        startActivity(launchWeb);
-    }
-    public void MyWeb2(View view)
-    {
-        openUrl2("https://manavata.org/health-project/");
-    }
-    public void openUrl2 (String url)
-    {
-        Uri uri=Uri.parse(url);
-        Intent launchWeb=new Intent(Intent. ACTION_VIEW, uri);
-        startActivity(launchWeb);
-    }
-    public void MyWeb3(View view)
-    {
-        openUrl3("https://manavata.org/university-project/");
-    }
-    public void openUrl3 (String url)
-    {
-        Uri uri=Uri.parse(url);
-        Intent launchWeb=new Intent(Intent. ACTION_VIEW, uri);
-        startActivity(launchWeb);
-    }
-    public void MyWeb4(View view)
-    {
-        openUrl4("https://manavata.org/ashrams/");
-    }
-    public void openUrl4 (String url)
-    {
-        Uri uri=Uri.parse(url);
-        Intent launchWeb=new Intent(Intent. ACTION_VIEW, uri);
-        startActivity(launchWeb);
-    }
-    public void MyWeb5(View view)
-    {
-        openUrl5("https://manavata.org/help-needy/");
-    }
-    public void openUrl5 (String url)
-    {
-        Uri uri=Uri.parse(url);
-        Intent launchWeb=new Intent(Intent. ACTION_VIEW, uri);
-        startActivity(launchWeb);
-    }
-    public void MyWeb6(View view)
-    {
-        openUrl6("https://manavata.org/environment/");
-    }
-    public void openUrl6 (String url)
-    {
-        Uri uri=Uri.parse(url);
-        Intent launchWeb=new Intent(Intent. ACTION_VIEW, uri);
+
+    private View.OnClickListener buttonClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()) {
+                case R.id.samskaravidya:
+                    openUrl("https://manavata.org/education-project/");
+                    break;
+                case R.id.yogaandhealthcare:
+                    openUrl("https://manavata.org/health-project/");
+                    break;
+                case R.id.universityforhumanity:
+                    openUrl("https://manavata.org/university-project/");
+                    break;
+                case R.id.ashramandchildcare:
+                    openUrl("https://manavata.org/ashrams/");
+                    break;
+                case R.id.helpneedy:
+                    openUrl("https://manavata.org/help-needy/");
+                    break;
+
+                case R.id.climatechange:
+                    openUrl("https://manavata.org/environment/");
+                    break;
+            }
+        }
+    };
+
+    public void openUrl(String url) {
+        Uri uri = Uri.parse(url);
+        Intent launchWeb = new Intent(Intent.ACTION_VIEW, uri);
         startActivity(launchWeb);
     }
 
 }
-
