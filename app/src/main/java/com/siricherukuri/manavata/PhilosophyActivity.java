dd5f424d661644600dd9f4416adaa0b66ea4cb53
Neerajakshi Daggubati <neerajakshid@users.noreply.github.com>
2021-01-30 19:23:01 -0800

    Update PhilosophyActivity.java

--- app/src/main/java/com/siricherukuri/manavata/PhilosophyActivity.java
+++ app/src/main/java/com/siricherukuri/manavata/PhilosophyActivity.java
@@ -2,10 +2,11 @@ package com.siricherukuri.manavata;
 
 import android.os.Bundle;
 
-public class MissionActivity extends MainActivity {
+public class PhilosophyActivity extends MainActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_mission);
+        setContentView(R.layout.activity_philosophy);
     }
+
 }
