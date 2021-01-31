fc3b92fb9599e303cd082fe44874745f397e26fd
Neerajakshi Daggubati <neerajakshid@users.noreply.github.com>
2021-01-30 18:55:25 -0800

    Update MissionActivity.java

--- app/src/main/java/com/siricherukuri/manavata/MissionActivity.java
+++ app/src/main/java/com/siricherukuri/manavata/MissionActivity.java
@@ -1,37 +1,11 @@
 package com.siricherukuri.manavata;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 public class MissionActivity extends MainActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mission);
-
-        defineTextViews();
     }
-    public void defineTextViews() {
-        findViewById(R.id.mission).setOnClickListener(buttonClickListener);
-        findViewById(R.id.missionDefine).setOnClickListener(buttonClickListener);
-        findViewById(R.id.missionTitle).setOnClickListener(buttonClickListener);
-    }
-
-    private View.OnClickListener buttonClickListener = new View.OnClickListener() {
-        @Override
-        public void onClick (View view) {
-            switch (view.getId()) {
-                case R.id.mission:
-                    startActivity(new Intent(MissionActivity.this, MissionInfo.class));
-                    break;
-                case R.id.missionDefine:
-                    startActivity(new Intent(MissionActivity.this, MissionInfo.class));
-                    break;
-                case R.id.missionTitle:
-                    startActivity(new Intent(MissionActivity.this, MissionInfo.class));
-                    break;
-            }
-        }
-    };
 }
